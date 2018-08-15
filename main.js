fc750d4dd7ae38193fb6afd0e16c0989b82a2e2a
Adnan Kamili <adnan.kamili@gmail.com>
2018-08-15 17:52:05 +0530

    added existing user check

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const uuid = require('uuid');
 const parse = require('csv-parse/lib/sync')
 const fetch = require('node-fetch')
 
@@ -7,6 +8,35 @@ const apiBaseUrl = "https://api.cryptlex.com/v3";
 const productId = 'PASTE_YOUR_PRODUCT_ID';
 
 const csvFilePath = 'sample.csv';
+
+async function createUser(url, resource) {
+    // check whether user exists
+    let users = [];
+    console.log("fetching existing user...");
+    const response = await fetch(`${url}?email=${resource.email}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        }
+    });
+    if (response.status == 200) {
+        users = await response.json();
+    } else {
+        console.error(response.status, resource, await response.json());
+    }
+    if (users.length) {
+        console.log("user already exists!")
+        return users[0];
+    }
+    console.log("user not found, creating new user...")
+    // create a new user
+    const user = await createResource(url, resource);
+    if(user){
+        console.log("user created:", user.name);
+    }
+    return user;
+}
 
 async function createResource(url, resource) {
     const response = await fetch(url, {
@@ -43,7 +73,7 @@ async function importCsv(filePath) {
             }
             // assuming csv contains some order_id
             if (row.order_id) {
-                licenseBody.metadata.push({ key: 'order_id', value: row.order_id, visible: false });
+                licenseBody.metadata.push({ key: 'order_id', value: row.order_id, visible: true });
             }
 
             // check for user details in csv
@@ -52,20 +82,19 @@ async function importCsv(filePath) {
                     email: row.email,
                     firstName: row.firstName,
                     lastName: row.lastName,
-                    password: 'top_secret', // add your logic for password generation
+                    password: uuid.v4(), // add your logic for password generation
                     roles: ['user']
                 }
                 console.log("creating user...")
-                const user = await createResource(`${apiBaseUrl}/users`, userBody);
+                const user = await createUser(`${apiBaseUrl}/users`, userBody);
                 if (user) {
-                    console.log("user created:", user.name);
                     licenseBody.userId = user.id;
                 }
             }
             console.log("creating license...")
             const license = await createResource(`${apiBaseUrl}/licenses`, licenseBody);
             if(license) {
-                console.log("license created:", license.key);
+                console.log("license created:", license);
             }
         }
     } catch (error) {
